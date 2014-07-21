6c9f291f9606a92e37da4950b0f1e8ccc0169f79
Andrew Mulholland <andrew@bash.sh>
2014-07-21 17:51:17 -0400

    add dependency on apache http client add new class for uploading images (currently empty)

--- src/main/java/sh/bash/imageproc/CarRecognition.java
+++ src/main/java/sh/bash/imageproc/CarRecognition.java
@@ -3,7 +3,9 @@ package sh.bash.imageproc;
 /**
  * Created by Andrew Mulholland on 21/07/2014.
  *
- * This is part of a process to
+ * This is part of a process to analyse images of cars, and classify them by manufacturer using OpenCV and respond with
+ * that classification.
+ *
  */
 public class CarRecognition {
     public static void main(String args[]) {
---
+++ src/main/java/sh/bash/imageproc/ImageUpload.java
@@ -0,0 +1,8 @@
+package sh.bash.imageproc;
+
+
+/**
+ * Created by andy on 21/07/2014.
+ */
+public class ImageUpload {
+}
